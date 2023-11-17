81bfc2b1f1a5bf785b335536b6bad7be1ab0e0b6
Riyan Anosh <riyananosh@gmail.com>
2023-11-16 18:52:30 -0800

    maim

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import time
 import pytesseract
 from threading import Thread
 import cv2
-
+import serial
+ser = serial.Serial('/dev/ttyACM0')
+ser.baudrate = 9000
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 while(vc.isOpened()):
@@ -16,13 +18,17 @@ while(vc.isOpened()):
         file.write(text)
         print(text)
         file.close()
+        ser.write(bytes(text,'utf-8'))
+        time.sleep(3)
     scan = Thread(target=reader)
     scan.start()
     cv2.imshow('Video',frame)
     time.sleep(0.1)
-    scan.join
     if cv2.waitKey(1) & 0xFF ==ord('e'):
         break
+    time.sleep(0.15)
+    scan.join
+
 
 vc.release()
 cv2.destroyWindow("preview")
